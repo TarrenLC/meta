2c09d7204a9fe3bab79b978f8aaa4ebbc01f3fbe
TarrenLC <tarren.leon@gmail.com>
2024-06-24 11:11:20 +1000

    24/06/24

--- conduct_sensitivity_rho.R
+++ conduct_sensitivity_rho.R
@@ -1,15 +1,18 @@
+#' `conduct_sensitivity_rho()` computes a pooled effect size using the information from "effect_sizes.csv" by 
+#' fitting a multilevel meta-analytic model (applying robust variance estimation) and using different values of rho.
+#' @param dat A data frame containing effect sizes (i.e., "effect_sizes.csv")
+#' @param rho_vec numeric vector of values of the correlation of the sampling errors within clusters
+#' @returns  a data frame with summary information of the pooled estimates
+#' @examples
+#' 
+# Adapted from https://github.com/cdsbasel/cumulative/blob/main/code/functions/conduct_sensitivity_rho.R
 
 # library(tidyverse) # for data wrangling
 # library(metafor) # compute effect sizes
 
 conduct_sensitivity_rho <- function(dat, rho_vec) {
   
-  #' `conduct_sensitivity_rho()` computes a pooled effect size using the information from "effect_sizes.csv" by 
-  #' fitting a multilevel meta-analytic model (applying robust variance estimation) and using different values of rho.
-  #' @param dat A data frame containing effect sizes (i.e., "effect_sizes.csv")
-  #' @param rho_vec numeric vector of values of the correlation of the sampling errors within clusters
-  #' @returns  a data frame with summary information of the pooled estimates
-  #' @examples
+
   
   # get the mlma function
   source("conduct_mlma.R")
